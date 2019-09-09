23810f760d8ed96cf3b9d2db6b5c2e12093c44fd
Syofyan Zuhad <sofyanzuhad2@gmail.com>
2019-09-09 20:54:21 +0700

    adding security login with session

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 
+if (isset($_SESSION['nama'])) {
+  if (!empty($_SESSION['nama'])) {
+    header('Location: Beranda.php');
+  }
+}
+
 require('Controller log.php');
 if (isset($_POST['Daftar'])) {
   $nama = $_POST['Name'];
